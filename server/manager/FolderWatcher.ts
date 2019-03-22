e8eabbb12b2776c4de993300169318ff5a55dfce
Harishkumar Gangula <harishg@ilimi.in>
2019-03-22 16:53:43 +0530

    Issue #SB-10822 feat:  added folder watcher

---
+++ server/manager/FolderWatcher.ts
@@ -0,0 +1,20 @@
+import watchr from 'watchr';
+
+
+export default class FolderWatcher {
+
+    private watchers: any[] = [];
+    private pluginId: string;
+
+    initialize(pluginId) {
+        this.pluginId = pluginId;
+    }
+
+    addWatcher(folderPath, listener, callback) {
+        this.watchers[this.pluginId + '_' + folderPath] = watchr.open(folderPath, listener, callback)
+    }
+
+    removeWatcher(folderPath) {
+        this.watchers[this.pluginId + '_' + folderPath].close()
+    }
+}
