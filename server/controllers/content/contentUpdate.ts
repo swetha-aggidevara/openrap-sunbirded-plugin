68a8d89cff60c05b22c1db9bda904c8ef487e426
Sourav Dey <sourav_d@techjoomla.com>
2019-09-27 21:21:14 +0530

    Issue #SB-13991 feat: Collection update

--- server/controllers/content/contentUpdate.ts
+++ server/controllers/content/contentUpdate.ts
@@ -7,6 +7,7 @@ import { logger } from '@project-sunbird/ext-framework-server/logger';
 import { containerAPI } from "OpenRAP/dist/api";
 import { HTTPService } from "@project-sunbird/ext-framework-server/services";
 import { CONTENT_DOWNLOAD_STATUS } from './contentDownload';
+import { diff, addedDiff, deletedDiff, updatedDiff, detailedDiff } from 'deep-object-diff';
 
 let dbName = "content_download";
 export default class ContentDownload {
@@ -30,20 +31,23 @@ export default class ContentDownload {
             if (parentId && _.get(liveContentData, 'data.result.content.mimeType') !== "application/vnd.ekstep.content-collection" && _.get(liveContentData, 'data.result.content.pkgVersion') > localContentData.pkgVersion) {
                 // Resource update inside collection
                 logger.debug(`Resource Id inside collection = "${id}" for content update`);
-                let a = await this.resourceInsideCollectionUpdate(parentId, liveContentData).then(data => { return res.send(Response.success('api.content.update', data)); });
-                return a;
+                await this.resourceInsideCollectionUpdate(parentId, liveContentData).then(data => {
+                    return res.send(Response.success('api.content.update', data));
+                });
             }
             else if (_.get(liveContentData, 'data.result.content.mimeType') === "application/vnd.ekstep.content-collection" && _.get(liveContentData, 'data.result.content.pkgVersion') > localContentData.pkgVersion) {
-
-                // Todo collection update
+                // Collection update
                 logger.debug(`Collection Id = "${id}" for content update`);
-
+                await this.collectionUpdate(localContentData, liveContentData).then(data => {
+                    return res.send(Response.success('api.content.update', data));
+                });
             }
             else if (_.get(liveContentData, 'data.result.content.mimeType') !== "application/vnd.ekstep.content-collection" && _.get(liveContentData, 'data.result.content.pkgVersion') > localContentData.pkgVersion) {
                 // Resource update
                 logger.debug(`Resource Id = "${id}" for content update`);
-                let a = await this.resourceUpdate(liveContentData).then(data => { return res.send(Response.success('api.content.update', data)); });
-                return a;
+                await this.resourceUpdate(liveContentData).then(data => {
+                    return res.send(Response.success('api.content.update', data));
+                });
             }
             else {
                 logger.error(`ReqId = "${req.headers['X-msgid']}": Received error while processing content update for content ${req.params.id}`);
@@ -62,34 +66,158 @@ export default class ContentDownload {
             try {
                 let downloadManager = containerAPI.getDownloadManagerInstance(this.pluginId);
                 let parentContentData = await HTTPService.get(`${process.env.APP_BASE_URL}/api/content/v1/read/${parentId}`, {}).toPromise();
-                        let downloadFiles = [{
-                            id: (_.get(liveContentData, "data.result.content.identifier") as string),
-                            url: (_.get(liveContentData, "data.result.content.downloadUrl") as string),
-                            size: (_.get(liveContentData, "data.result.content.size") as number)
-                        }];
-                        let downloadId = await downloadManager.download(downloadFiles, 'ecars');
-                        let queueMetaData = {
-                            mimeType: _.get(parentContentData, 'data.result.content.mimeType'),
-                            items: downloadFiles,
-                            pkgVersion: _.get(parentContentData, 'data.result.content.pkgVersion'),
-                            contentType: _.get(parentContentData, 'data.result.content.contentType'),
-                        }
-                        logger.debug(`insert to the content_download for content update`);
-                        await this.databaseSdk.insert(dbName, {
-                            downloadId: downloadId,
-                            contentId: _.get(parentContentData, "data.result.content.identifier"),
-                            name: _.get(parentContentData, "data.result.content.name"),
-                            status: CONTENT_DOWNLOAD_STATUS.Submitted,
-                            queueMetaData: queueMetaData,
-                            createdOn: Date.now(),
-                            updatedOn: Date.now()
-                        })
-                        logger.info(`Resource inserted in database successfully for content update`);
-                        resolve(downloadId);
+                let downloadFiles = [{
+                    id: (_.get(liveContentData, "data.result.content.identifier") as string),
+                    url: (_.get(liveContentData, "data.result.content.downloadUrl") as string),
+                    size: (_.get(liveContentData, "data.result.content.size") as number)
+                }];
+                let downloadId = await downloadManager.download(downloadFiles, 'ecars');
+                let queueMetaData = {
+                    mimeType: _.get(parentContentData, 'data.result.content.mimeType'),
+                    items: downloadFiles,
+                    pkgVersion: _.get(parentContentData, 'data.result.content.pkgVersion'),
+                    contentType: _.get(parentContentData, 'data.result.content.contentType'),
+                }
+                logger.debug(`Resource inside collection insert to the content_download for content update`);
+                await this.databaseSdk.insert(dbName, {
+                    downloadId: downloadId,
+                    contentId: _.get(parentContentData, "data.result.content.identifier"),
+                    name: _.get(parentContentData, "data.result.content.name"),
+                    status: CONTENT_DOWNLOAD_STATUS.Submitted,
+                    queueMetaData: queueMetaData,
+                    createdOn: Date.now(),
+                    updatedOn: Date.now()
+                })
+                logger.info(`Resource inserted in database successfully for content update`);
+                resolve(downloadId);
             } catch (err) {
                 reject(err);
             }
         })
+    }
+
+    collectionUpdate(localContentData, liveContentData) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let downloadManager = containerAPI.getDownloadManagerInstance(this.pluginId);
+                let downloadFiles = [{
+                    id: (_.get(liveContentData, "data.result.content.identifier") as string),
+                    url: (_.get(liveContentData, "data.result.content.downloadUrl") as string),
+                    size: (_.get(liveContentData, "data.result.content.size") as number)
+                }];
+
+                // Get the local child contents
+                const localChildNodes = _.get(localContentData, "childNodes");
+                if (!_.isEmpty(localChildNodes)) {
+                    let { docs: localChildContents = [] } = await this.databaseSdk.find('content', {
+                        selector: {
+                            $and: [
+                                {
+                                    _id: {
+                                        $in: localChildNodes
+                                    }
+                                },
+                                {
+                                    mimeType: {
+                                        $nin: ['application/vnd.ekstep.content-collection']
+                                    }
+                                }
+                            ]
+                        }
+                    });
+
+                    // Get the live child contents
+                    let liveChildNodes = _.get(liveContentData, "data.result.content.childNodes");
+                    if (!_.isEmpty(liveChildNodes)) {
+                        let liveChildrenContentsRes = await HTTPService.post(`${process.env.APP_BASE_URL}/api/content/v1/search`,
+                            {
+                                "request": {
+                                    "filters": {
+                                        "identifier": liveChildNodes,
+                                        "mimeType": { "!=": "application/vnd.ekstep.content-collection" }
+                                    },
+                                    "limit": liveChildNodes.length
+                                }
+                            }, {
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        }).toPromise();
+
+                        if (_.get(liveChildrenContentsRes, 'data.result.count')) {
+                            let liveChildcontents = _.get(liveChildrenContentsRes, 'data.result.content');
+                            let deletedIds = this.getDeletedContents(localChildContents, liveChildcontents);
+                            let addedAndUpdatedIds = this.getAddedAndUpdatedContents(liveChildcontents, localChildContents);
+
+                            // Updating visibilty to Default for deleted resources
+                            _.forEach(localChildContents, (data) => {
+                                if (_.includes(deletedIds, _.get(data, "_id"))) {
+                                    data.visibility = "Default"
+                                }
+                            });
+                            await this.databaseSdk.bulk('content', localChildContents);
+
+                            // Pushing downloadable childs to downloadfiles array
+                            for (let content of liveChildcontents) {
+                                if (_.includes(addedAndUpdatedIds, _.get(content, "identifier"))) {
+                                    downloadFiles.push({
+                                        id: (_.get(content, "identifier") as string),
+                                        url: (_.get(content, "downloadUrl") as string),
+                                        size: (_.get(content, "size") as number)
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+
+                let downloadId = await downloadManager.download(downloadFiles, 'ecars')
+                let queueMetaData = {
+                    mimeType: _.get(liveContentData, 'data.result.content.mimeType'),
+                    items: downloadFiles,
+                    pkgVersion: _.get(liveContentData, 'data.result.content.pkgVersion'),
+                    contentType: _.get(liveContentData, 'data.result.content.contentType'),
+                }
+                await this.databaseSdk.insert(dbName, {
+                    downloadId: downloadId,
+                    contentId: _.get(liveContentData, "data.result.content.identifier"),
+                    name: _.get(liveContentData, "data.result.content.name"),
+                    status: CONTENT_DOWNLOAD_STATUS.Submitted,
+                    queueMetaData: queueMetaData,
+                    createdOn: Date.now(),
+                    updatedOn: Date.now()
+                });
+                logger.info(`Collection inserted in database successfully for content update`);
+                resolve(downloadId);
+            } catch (err) {
+                reject(err);
+            }
+        })
+    }
+
+    getAddedAndUpdatedContents(liveContents, localContents) {
+        var contents = _.filter(liveContents, (data) => {
+            var b = _.find(localContents, { _id: data.identifier, pkgVersion: data.pkgVersion })
+            if (!b) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        return _.map(contents, 'identifier');
+    }
+
+    getDeletedContents(localContents, liveContents) {
+        var contents = _.filter(localContents, (data) => {
+            var b = _.find(liveContents, { identifier: data._id });
+            if (!b) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        return _.map(contents, 'identifier');
     }
 
     resourceUpdate(liveContentData) {
@@ -108,7 +236,7 @@ export default class ContentDownload {
                     pkgVersion: _.get(liveContentData, 'data.result.content.pkgVersion'),
                     contentType: _.get(liveContentData, 'data.result.content.contentType'),
                 }
-                logger.debug(`Insert to the content_download for content update`);
+                logger.debug(`Resource insert to the content_download for content update`);
                 await this.databaseSdk.insert(dbName, {
                     downloadId: downloadId,
                     contentId: _.get(liveContentData, "data.result.content.identifier"),
