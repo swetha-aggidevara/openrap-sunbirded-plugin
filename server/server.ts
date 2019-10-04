2a68932ee363695344278411acc22a20c6e49751
Harishkumar Gangula <harishg@ilimi.in>
2019-10-04 14:35:34 +0530

    Issue #SB-15083 fix: Telemetry SDK integrated

--- server/server.ts
+++ server/server.ts
@@ -1,142 +1,166 @@
-
-import { Manifest, BaseServer } from '@project-sunbird/ext-framework-server/models';
-import { frameworkAPI } from '@project-sunbird/ext-framework-server/api';
-import * as path from 'path';
-import { Inject } from 'typescript-ioc';
-import ContentManager from './manager/ContentManager'
-import { Framework } from './controllers/framework';
-import { Organization } from './controllers/organization';
-import { Page } from './controllers/page';
-import { ResourceBundle } from './controllers/resourceBundle';
-import { Channel } from './controllers/channel';
-import { Form } from './controllers/form';
-import DatabaseSDK from './sdk/database';
-import config from './config'
-import { logger } from '@project-sunbird/ext-framework-server/logger';
-import { containerAPI } from 'OpenRAP/dist/api';
-import { addContentListener, reconciliation } from './controllers/content/contentHelper';
-import { TelemetryService } from "./services";
-import * as _ from 'lodash';
-import { EventManager } from '@project-sunbird/ext-framework-server/managers/EventManager';
+import {
+  Manifest,
+  BaseServer
+} from "@project-sunbird/ext-framework-server/models";
+import { frameworkAPI } from "@project-sunbird/ext-framework-server/api";
+import * as path from "path";
+import { Inject } from "typescript-ioc";
+import ContentManager from "./manager/ContentManager";
+import { Framework } from "./controllers/framework";
+import { Organization } from "./controllers/organization";
+import { Page } from "./controllers/page";
+import { ResourceBundle } from "./controllers/resourceBundle";
+import { Channel } from "./controllers/channel";
+import { Form } from "./controllers/form";
+import DatabaseSDK from "./sdk/database";
+import config from "./config";
+import { logger } from "@project-sunbird/ext-framework-server/logger";
+import { containerAPI } from "OpenRAP/dist/api";
+import {
+  addContentListener,
+  reconciliation
+} from "./controllers/content/contentHelper";
+import * as _ from "lodash";
+import { EventManager } from "@project-sunbird/ext-framework-server/managers/EventManager";
 
 export class Server extends BaseServer {
+  private sunbirded_plugin_initialized = false;
+  private ecarsFolderPath: string = "ecars";
+  private contentFilesPath: string = "content";
+  private tempPath: string = "temp";
+  private telemetryArchivedFolderPath: string = "telemetry_archived";
 
-    private sunbirded_plugin_initialized = false;
-    private ecarsFolderPath: string = 'ecars';
-    private contentFilesPath: string = 'content';
-    private tempPath: string = 'temp';
-    private telemetryArchivedFolderPath: string = 'telemetry_archived';
+  @Inject
+  private databaseSdk: DatabaseSDK;
 
-    @Inject
-    private databaseSdk: DatabaseSDK;
+  @Inject
+  private contentManager: ContentManager;
 
-    @Inject
-    private telemetryService: TelemetryService;
+  @Inject
+  private fileSDK;
 
-    @Inject
-    private contentManager: ContentManager;
+  constructor(manifest: Manifest) {
+    super(manifest);
 
-    @Inject
-    private fileSDK;
+    // Added timeout since db creation is async and it is taking time and insertion is failing
+    this.fileSDK = containerAPI.getFileSDKInstance(manifest.id);
 
-    constructor(manifest: Manifest) {
-        super(manifest);
+    this.initialize(manifest)
+      .then(() => {
+        this.sunbirded_plugin_initialized = true;
+        EventManager.emit(`${manifest.id}:initialized`, {});
+      })
+      .catch(err => {
+        logger.error(
+          "Error while initializing open rap sunbird ed plugin",
+          err
+        );
+        this.sunbirded_plugin_initialized = true;
+        EventManager.emit(`${manifest.id}:initialized`, {});
+      });
+  }
+  async initialize(manifest: Manifest) {
+    //registerAcrossAllSDKS()
+    this.databaseSdk.initialize(manifest.id);
 
-        // Added timeout since db creation is async and it is taking time and insertion is failing
-        this.fileSDK = containerAPI.getFileSDKInstance(manifest.id);
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.contentFilesPath),
+      "/contentPlayer/preview/content"
+    );
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.contentFilesPath),
+      "/contentPlayer/preview"
+    );
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.contentFilesPath),
+      "/contentPlayer/preview/content/*/content-plugins"
+    );
+    frameworkAPI.registerStaticRoute(
+      path.join(__dirname, "..", "..", "public", "contentPlayer", "preview"),
+      "/contentPlayer/preview"
+    );
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.contentFilesPath),
+      "/content"
+    );
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.ecarsFolderPath),
+      "/ecars"
+    );
+    frameworkAPI.registerStaticRoute(
+      this.fileSDK.getAbsPath(this.tempPath),
+      "/temp"
+    );
+    frameworkAPI.registerStaticRoute(
+      path.join(__dirname, "..", "..", "public", "portal")
+    );
+    frameworkAPI.registerStaticRoute(
+      path.join(__dirname, "..", "..", "public", "sunbird-plugins"),
+      "/sunbird-plugins"
+    );
+    frameworkAPI.setStaticViewEngine("ejs");
 
+    // insert meta data for app
+    await this.insertConfig(manifest);
 
-        this.initialize(manifest)
-        .then(() => {
-            this.sunbirded_plugin_initialized = true;
-            EventManager.emit(`${manifest.id}:initialized`, {});
-        })
-        .catch(err => {
-            logger.error("Error while initializing open rap sunbird ed plugin", err);
-            this.sunbirded_plugin_initialized = true;
-            EventManager.emit(`${manifest.id}:initialized`, {});
-        })
+    const pluginConfig = {
+      pluginVer: manifest.version,
+      apiToken: process.env.APP_BASE_URL_TOKEN,
+      apiBaseURL: process.env.APP_BASE_URL,
+      apiTokenRefreshFn: "refreshToken"
+    };
+    await containerAPI.register(manifest.id, pluginConfig);
 
+    await this.fileSDK.mkdir(this.contentFilesPath);
+    await this.fileSDK.mkdir(this.ecarsFolderPath);
+    await this.fileSDK.mkdir(this.telemetryArchivedFolderPath);
 
-    }
-    async initialize(manifest: Manifest) {
+    // listener to index content when content downloaded
+    addContentListener(manifest.id);
+    reconciliation(manifest.id);
 
-        //registerAcrossAllSDKS()
-        this.databaseSdk.initialize(manifest.id);
-
-
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.contentFilesPath), '/contentPlayer/preview/content');
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.contentFilesPath), '/contentPlayer/preview');
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.contentFilesPath), '/contentPlayer/preview/content/*/content-plugins');
-        frameworkAPI.registerStaticRoute(path.join(__dirname, '..', '..', 'public', 'contentPlayer', 'preview'), '/contentPlayer/preview');
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.contentFilesPath), '/content');
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.ecarsFolderPath), '/ecars');
-        frameworkAPI.registerStaticRoute(this.fileSDK.getAbsPath(this.tempPath), '/temp');
-        frameworkAPI.registerStaticRoute(path.join(__dirname, '..', '..', 'public', 'portal'));
-        frameworkAPI.registerStaticRoute(path.join(__dirname, '..', '..', 'public', 'sunbird-plugins'), '/sunbird-plugins');
-        frameworkAPI.setStaticViewEngine('ejs');
-
-        // insert meta data for app
-        await this.insertConfig(manifest)
-
-        await this.telemetryService.initialize(manifest.id);
-        const pluginConfig = {
-            pluginVer: manifest.version,
-            apiToken: process.env.APP_BASE_URL_TOKEN,
-            apiBaseURL: process.env.APP_BASE_URL,
-            apiTokenRefreshFn: 'refreshToken'
-        }
-        await containerAPI.register(manifest.id, pluginConfig);
-
-        await this.fileSDK.mkdir(this.contentFilesPath)
-        await this.fileSDK.mkdir(this.ecarsFolderPath)
-        await this.fileSDK.mkdir(this.telemetryArchivedFolderPath)
-
-
-
-        // listener to index content when content downloaded
-        addContentListener(manifest.id);
-        reconciliation(manifest.id)
-
-        /* used to listen for content added to downloads folder and unzip them to 
+    /* used to listen for content added to downloads folder and unzip them to 
             content_files
             and inserts metadata to content database
         */
-        this.contentManager.initialize(manifest.id, this.fileSDK.getAbsPath(this.contentFilesPath),
-            this.fileSDK.getAbsPath(this.ecarsFolderPath))
+    this.contentManager.initialize(
+      manifest.id,
+      this.fileSDK.getAbsPath(this.contentFilesPath),
+      this.fileSDK.getAbsPath(this.ecarsFolderPath)
+    );
 
+    // delete contents in temp directory
+    await this.fileSDK
+      .remove("temp")
+      .catch(err =>
+        logger.error(`while emptying temp folder on startup ${err}`)
+      );
+    //- reIndex()
+    //- reConfigure()
+  }
 
-        // delete contents in temp directory
-        await this.fileSDK.remove('temp').catch(err => logger.error(`while emptying temp folder on startup ${err}`))
-        //- reIndex()
-        //- reConfigure()
-    }
+  private async insertConfig(manifest: Manifest) {
+    const framework = new Framework(manifest);
+    const organization = new Organization(manifest);
+    const page = new Page(manifest);
+    const resourceBundle = new ResourceBundle(manifest);
+    const channel = new Channel(manifest);
+    const form = new Form(manifest);
 
-    private async insertConfig(manifest: Manifest) {
-        const framework = new Framework(manifest);
-        const organization = new Organization(manifest);
-        const page = new Page(manifest);
-        const resourceBundle = new ResourceBundle(manifest);
-        const channel = new Channel(manifest);
-        const form = new Form(manifest);
-
-        await organization.insert();
-        await resourceBundle.insert();
-        await framework.insert();
-        await channel.insert();
-        await form.insert();
-        await page.insert();
-
-    }
-
+    await organization.insert();
+    await resourceBundle.insert();
+    await framework.insert();
+    await channel.insert();
+    await form.insert();
+    await page.insert();
+  }
 }
 
 process
-    .on('unhandledRejection', (reason, p) => {
-        logger.error(reason, 'Unhandled Rejection at Promise', p);
-    })
-    .on('uncaughtException', err => {
-        logger.error(err, 'Uncaught Exception thrown');
-        process.exit(1);
-    });
-
+  .on("unhandledRejection", (reason, p) => {
+    logger.error(reason, "Unhandled Rejection at Promise", p);
+  })
+  .on("uncaughtException", err => {
+    logger.error(err, "Uncaught Exception thrown");
+    process.exit(1);
+  });
