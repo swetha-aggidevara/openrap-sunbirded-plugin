8afb2c9dffabe139c8a44cafe9b773d186779712
Anoop HM <anoophm44@gmail.com>
2019-10-16 13:27:05 +0530

    Issue #SB-14823 feat: extracting and processing manifest file

--- server/manager/contentImportManager/contentImportHelper.ts
+++ server/manager/contentImportManager/contentImportHelper.ts
@@ -1,37 +1,95 @@
-const ecarFileName = "Science - Part 2.ecar";
+// const ecarFileName = "Science - Part 2.ecar";
 // const ecarFileName = "10 ಗಣಿತ ಭಾಗ 1.ecar";
 const contentFolder = "./content/";
 const ecarFolder = "./ecar/";
 import * as  StreamZip from 'node-stream-zip';
 import * as  fs from 'fs';
 import * as  _ from 'lodash';
-let ecarEntries = {};
+let zipHandler;
 
-const extractEcar = async (data) => {
-  const ecarFileName = data.ecarFileName
-  ecarEntries = data.ecarEntries
-  if(!ecarFileName.endsWith('.ecar')){
-    throw "INVALID_FILE_EXTENSION";
-  }
-  let zipHandler;
-  let contentBasePath = contentFolder + ecarFileName.substr(0, ecarFileName.length - 5)
-  let ecarBasePath = ecarFolder + ecarFileName;
-  console.log('contentBasePath and ecarBasePath in extractEcar', contentBasePath, ecarBasePath);
-  console.log('ecarEntries in extractEcar', ecarEntries);
+enum ImportSteps {
+  copyEcar = "COPY_ECAR",
+  parseEcar = "PARSE_ECAR",
+  processManifest = "PROCESS_MANIFEST",
+  extractEcar = "EXTRACT_ECAR",
+  processContent = "PROCESS_CONTENT",
+  saveContent = "SAVE_CONTENT"
+}
+
+enum ImportStatus {
+  inQueue = "IN_QUEUE",
+  resume = "RESUME",
+  inProgress = "IN_PROGRESS",
+  paused = "PAUSED",
+  completed = "COMPLETED",
+  failed = "FAILED",
+  canceled = "CANCELED",
+  reconcile = "RECONCILE"
+}
+
+interface IContentImport {
+  id: string;
+  importStatus: ImportStatus;
+  createdOn: string | number;
+  ecarSourcePath: string;
+  contentId?: string;
+  contentType?: string;
+  importStep?: ImportSteps;
+  ecarContentEntries?: Object;
+  extractedEntries?: Object;
+  contentToBeAdded?: Object;
+  contentToBeUpdated?: Object;
+  failedReason?: string;
+}
+const copyEcar = (contentImportData: IContentImport) => {
+  console.log('copping ecar from src location to ecar folder', contentImportData.ecarSourcePath);
+  fs.copyFile(contentImportData.ecarSourcePath, ecarFolder + contentImportData.id + '.ecar', (err) => { // TODO: check source ecar path before copying
+    if (err) {
+      process.send({message: "IMPORT_ERROR", err})
+    } else {
+      process.send({message: ImportSteps.copyEcar, contentImportData})
+    }
+  })
+}
+const parseEcar = async (contentImportData: IContentImport) => {
+  let ecarBasePath = ecarFolder + contentImportData.id + '.ecar';
+  let contentBasePath = contentFolder + contentImportData.id
   try {
     zipHandler = await loadZip(ecarBasePath);
     createDirectory(contentBasePath)
-    for (const entry of _.values(zipHandler.entries()) as any) {
-      if(!ecarEntries[entry.name]){
-        // console.log(`extracting Name: ${entry.name} `) //, contentBasePath,  ecarEntries[entry.name]) //, type: ${desc}, size: ${entry.size}`);
-        ecarEntries[entry.name] = true;
-        await extractContent(zipHandler, contentBasePath, entry)
-      }
+    contentImportData.ecarContentEntries = zipHandler.entries();
+    if(!contentImportData.ecarContentEntries['manifest.json']){
+      throw "MANIFEST_MISSING";
     }
-    process.send({message: "IMPORT_COMPLETE", ecarEntries})
+    await extractContent(zipHandler, contentBasePath, contentImportData.ecarContentEntries['manifest.json'])
+    process.send({message: ImportSteps.parseEcar, contentImportData})
   } catch (err) {
     console.log('error while importing ecar', err);
-    process.send({message: "EXTRACT_ERROR", err})
+    process.send({message: "IMPORT_ERROR", err})
+  }
+}
+const extractEcar = async (contentImportData: IContentImport) => {
+  let ecarBasePath = ecarFolder + contentImportData.id + '.ecar';
+  let contentBasePath = contentFolder + contentImportData.id
+  console.log('contentBasePath and ecarBasePath in extractEcar', contentBasePath, ecarBasePath);
+  console.log('ecarEntries in extractEcar', contentImportData.extractedEntries);
+  try {
+    if(!zipHandler){
+      zipHandler = await loadZip(ecarBasePath);
+    }
+    for (const entry of _.values(zipHandler.entries()) as any) {
+      if(!contentImportData.extractedEntries[entry.name]){
+        // console.log(`extracting Name: ${entry.name} `) //, contentBasePath,  ecarEntries[entry.name]) //, type: ${desc}, size: ${entry.size}`);
+        contentImportData.extractedEntries[entry.name] = true;
+        await extractContent(zipHandler, contentBasePath, entry)
+      } else {
+        console.log('entry extracted already', entry);
+      }
+    }
+    process.send({message: ImportSteps.extractEcar, contentImportData})
+  } catch (err) {
+    console.log('error while importing ecar', err);
+    process.send({message: "IMPORT_ERROR", err})
   } finally {
     console.log('extracted all contents');
     if(zipHandler.close){
@@ -66,10 +124,13 @@ const createDirectory = async (path) => {
 }
 
 process.on('message', (data) => {
-  if (data.message === "IMPORT") {
-    console.log(data)
-    extractEcar(data);
+  if (data.message === ImportSteps.copyEcar) {
+    copyEcar(data.contentImportData)
+  } else if(data.message === ImportSteps.parseEcar) {
+    parseEcar(data.contentImportData)
+  } else if (data.message === ImportSteps.extractEcar) {
+    extractEcar(data.contentImportData);
   } else if (data.message === "KILL") {
-    process.send({message: "DATA_SYNC_KILL", ecarEntries})
+    process.send({message: "DATA_SYNC_KILL", contentImportData: ''})
   }
 });
--- server/manager/contentImportManager/contentImportManager.ts
+++ server/manager/contentImportManager/contentImportManager.ts
@@ -2,13 +2,17 @@ import * as  _ from 'lodash';
 import * as uuid from 'uuid';
 import * as childProcess from 'child_process';
 import * as os from 'os';
-
+import * as  fs from 'fs';
+const contentFolder = "./content/";
+const ecarFolder = "./ecar/";
 console.info('System is running on', os.cpus().length, 'cpus');
-const maxRunningImportJobs = 1 || os.cpus().length; 
+const maxRunningImportJobs = 1 || os.cpus().length;
 let contentImportDB: Array<IContentImport> = [];
 
 enum ImportSteps {
-  loadManifest = "LOAD_MANIFEST",
+  copyEcar = "COPY_ECAR",
+  parseEcar = "PARSE_ECAR",
+  processManifest = "PROCESS_MANIFEST",
   extractEcar = "EXTRACT_ECAR",
   processContent = "PROCESS_CONTENT",
   saveContent = "SAVE_CONTENT"
@@ -36,8 +40,10 @@ interface IContentImport {
   createdOn: string | number;
   ecarSourcePath: string;
   contentId?: string;
+  contentType?: string;
   importStep?: ImportSteps;
   ecarContentEntries?: Object;
+  extractedEntries?: Object;
   contentToBeAdded?: Object;
   contentToBeUpdated?: Object;
   failedReason?: string;
@@ -50,11 +56,10 @@ export class ContentImportManager {
   this method will be called when app initializes, all task related to import should be called after this method completes
   it updates all in-progress state content to RECONCILE status
   then checkImportQueue will be called, checkImportQueue will pick RECONCILE on priority and completes import task the task
-
   */
-  public async reconcile(){
+  public async reconcile() {
     _.forEach(contentImportDB, (eachContent: IContentImport) => {
-      if(eachContent.importStatus === ImportStatus.inProgress){
+      if (eachContent.importStatus === ImportStatus.inProgress) {
         eachContent.importStatus = ImportStatus.reconcile
       }
     }); // for all in-progress task in db, update status to RECONCILE
@@ -64,7 +69,7 @@ export class ContentImportManager {
     console.info('registerImportJob started for ', ecarPaths);
     ecarPaths = await this.getUnregisteredEcars(ecarPaths)
     console.info('after unique check', ecarPaths);
-    if(!ecarPaths || !ecarPaths.length){
+    if (!ecarPaths || !ecarPaths.length) {
       console.debug('no unique ecar found, exiting registerImportJob');
       return [];
     }
@@ -72,65 +77,68 @@ export class ContentImportManager {
       id: uuid(),
       importStatus: ImportStatus.inQueue,
       createdOn: Date.now(),
-      ecarSourcePath: ecarPath
+      ecarSourcePath: ecarPath,
+      importStep: ImportSteps.copyEcar
     }))
     contentImportDB.push(...dbData); // insert to contentImport DB
     this.checkImportQueue();
     return dbData.map(data => data.id)
   }
 
-  private async checkImportQueue(status: Array<ImportStatus> = [ImportStatus.inQueue, ImportStatus.resume, ImportStatus.reconcile]) {
+  private async checkImportQueue(status: Array<ImportStatus> = [ImportStatus.reconcile, ImportStatus.resume, ImportStatus.inQueue]) {
     console.info('checkImportQueue method called', contentImportDB);
-    if(this.runningImportJobs.length >= maxRunningImportJobs){
+    if (this.runningImportJobs.length >= maxRunningImportJobs) {
       console.debug('no slot available to import, exiting');
       return;
     }
     const queuedJobs = _.filter(contentImportDB, job => _.includes(status, job.importStatus)) // get IN_QUEUE jobs from db
     console.info('list of queued jobs', queuedJobs);
-    if(!queuedJobs.length){
+    if (!queuedJobs.length) {
       console.debug('no queued jobs in db, exiting');
       return;
     }
     console.info('entering while loop', maxRunningImportJobs, this.runningImportJobs.length);
     let queuedJobIndex = 0;
-    while(maxRunningImportJobs > this.runningImportJobs.length && queuedJobs[queuedJobIndex]){
+    while (maxRunningImportJobs > this.runningImportJobs.length && queuedJobs[queuedJobIndex]) {
       console.info('in while loop', queuedJobs[queuedJobIndex], this.runningImportJobs.length);
-      const jobRunning: any = _.find(this.runningImportJobs, { id: queuedJobs[queuedJobIndex].id}); // duplicate check
-      if(!jobRunning){
+      const jobRunning: any = _.find(this.runningImportJobs, { id: queuedJobs[queuedJobIndex].id }); // duplicate check
+      if (!jobRunning) {
+        const jobReference = new ImportEcar(queuedJobs[queuedJobIndex], this.importJobCompletionCb.bind(this))
+        jobReference.startImport();
         this.runningImportJobs.push({
           id: queuedJobs[queuedJobIndex].id,
-          jobReference: new ImportEcar(queuedJobs[queuedJobIndex], this.importJobCompletionCb.bind(this))
+          jobReference
         })
       }
-      const importDbResults: IContentImport = _.find(contentImportDB, { id: queuedJobs[queuedJobIndex].id}); // find import job in db
+      const importDbResults: IContentImport = _.find(contentImportDB, { id: queuedJobs[queuedJobIndex].id }); // find import job in db
       importDbResults.importStatus = ImportStatus.inProgress // update status to in-progress in db
       queuedJobIndex++
     }
     console.info('exited while loop', queuedJobIndex, this.runningImportJobs.length);
   }
-  private importJobCompletionCb(err: any, data: IContentImport){
-    if(err){
+  private importJobCompletionCb(err: any, data: IContentImport) {
+    if (err) {
       console.error('error will importing content with id', data.id, 'err', err);
-      const importDbResults: IContentImport = _.find(contentImportDB, { id: data.id}); // find import job in db
+      const importDbResults: IContentImport = _.find(contentImportDB, { id: data.id }); // find import job in db
       importDbResults.importStatus = ImportStatus.failed // update status to failed in db
     } else {
-      console.log('completed import job for ',this.runningImportJobs.length, data);
+      console.log('completed import job for ', data.id, this.runningImportJobs.length);
       _.remove(this.runningImportJobs, job => job.id === data.id) // update meta data in db 
       console.log(this.runningImportJobs.length);
-      const importDbResults: IContentImport = _.find(contentImportDB, { id: data.id}); // find import job in db
+      const importDbResults: IContentImport = _.find(contentImportDB, { id: data.id }); // find import job in db
       importDbResults.importStatus = ImportStatus.completed // update status to completed in db
     }
     this.checkImportQueue()
   }
 
   public async pauseImport(importId: string) {
-    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId}); // find import job in db
-    if(_.includes([ImportStatus.canceled, ImportStatus.completed, ImportStatus.failed], importDbResults.importStatus)){
+    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId }); // find import job in db
+    if (_.includes([ImportStatus.canceled, ImportStatus.completed, ImportStatus.failed], importDbResults.importStatus)) {
       throw "INVALID_OPERATION"
     }
-    if(importDbResults.importStatus === ImportStatus.inProgress){
-      const inProgressJob: IRunningImportJobs = _.find(this.runningImportJobs, { id: importId}); // running/in-progress job
-      if(!inProgressJob){
+    if (importDbResults.importStatus === ImportStatus.inProgress) {
+      const inProgressJob: IRunningImportJobs = _.find(this.runningImportJobs, { id: importId }); // running/in-progress job
+      if (!inProgressJob) {
         throw "INVALID_OPERATION"
       }
       const data = await inProgressJob.jobReference.pause();
@@ -140,26 +148,26 @@ export class ContentImportManager {
   }
 
   public async resumeImport(importId: string) {
-    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId}); // find import job in db
-    if(!_.includes([ImportStatus.paused], importDbResults.importStatus)){
+    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId }); // find import job in db
+    if (!_.includes([ImportStatus.paused], importDbResults.importStatus)) {
       throw "INVALID_OPERATION"
     }
     importDbResults.importStatus = ImportStatus.resume; // update db with new status
     this.checkImportQueue();
   }
 
-  public async cancelImport(importId: string){
-    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId}); // find import job in db
-    if(_.includes([ImportStatus.canceled, ImportStatus.completed, ImportStatus.failed], importDbResults.importStatus)){
+  public async cancelImport(importId: string) {
+    const importDbResults: IContentImport = _.find(contentImportDB, { id: importId }); // find import job in db
+    if (_.includes([ImportStatus.canceled, ImportStatus.completed, ImportStatus.failed], importDbResults.importStatus)) {
       throw "INVALID_OPERATION"
     }
-    if(importDbResults.importStatus === ImportStatus.inProgress){
-      const inProgressJob: IRunningImportJobs = _.find(this.runningImportJobs, { id: importId}); // running/in-progress job
-      if(!inProgressJob){
+    if (importDbResults.importStatus === ImportStatus.inProgress) {
+      const inProgressJob: IRunningImportJobs = _.find(this.runningImportJobs, { id: importId }); // running/in-progress job
+      if (!inProgressJob) {
         throw "INVALID_OPERATION"
       }
       await inProgressJob.jobReference.cancel();
-      _.remove(this.runningImportJobs, job => job.id === inProgressJob.id) 
+      _.remove(this.runningImportJobs, job => job.id === inProgressJob.id)
     }
     importDbResults.importStatus = ImportStatus.canceled; // update db with new status
   }
@@ -167,7 +175,7 @@ export class ContentImportManager {
   private async getUnregisteredEcars(ecarPaths: Array<string>): Promise<Array<string>> {
     const registeredEcars = contentImportDB; // get IN_QUEUE, IN_PROGRESS jobs from db
     ecarPaths = _.filter(ecarPaths, ecarPath => {
-      if(_.find(registeredEcars, { ecarSourcePath: ecarPath })){
+      if (_.find(registeredEcars, { ecarSourcePath: ecarPath })) {
         console.log('skipping import for ', ecarPath, ' as its already registered');
         return false;
       } else {
@@ -188,44 +196,153 @@ class ImportEcar {
 
   workerProcessRef: childProcess.ChildProcess;
   contentManifest: any;
-  manifest: contentManifest = {
-    archive: {
-      items: []
-    }
-  }
-  constructor(private contentImportData: IContentImport, private cb) {
-    this.startImport()
-  }
-  startImport() {
+  manifest: contentManifest;
+
+  constructor(private contentImportData: IContentImport, private cb) { 
     this.workerProcessRef = childProcess.fork('./contentImportHelper');
     this.handleChildProcessMessage();
+    this.handleWorkerCloseEvents();
+  }
+
+  handleWorkerCloseEvents() {
     this.workerProcessRef.on('close', (data) => {
       console.log('worker close signal', data);
     });
     this.workerProcessRef.on('exit', (code, signal) => {
       console.log('worker exited:', code, signal);
     });
-    console.log('sending message to child');
-    this.workerProcessRef.send({
-      message: 'IMPORT',
-      ecarFileName: this.contentImportData.ecarSourcePath,
-      ecarEntries: this.contentImportData.ecarContentEntries || {}
-    });
   }
-  copyEcar(ecarFilePath) {
-
+  processContents(contentImportData){
+    this.cb(null, this.contentImportData);
   }
-
-  handleChildProcessMessage() {
-    this.workerProcessRef.on('message', (data) => {
+  async handleChildProcessMessage() {
+    this.workerProcessRef.on('message', async (data) => {
       console.log('got message from child', data.message);
-      if(data.message === "DATA_SYNC_KILL"){
-        this.contentImportData.ecarContentEntries = data.ecarEntries
+      if (data.message === ImportSteps.copyEcar) {
+        this.copyEcar()
+      } else if (data.message === ImportSteps.parseEcar) {
+        this.processManifest(data.contentImportData)
+      } else if (data.message === ImportSteps.extractEcar) {
+        this.processContents(data.contentImportData)
+      } else if (data.message === "DATA_SYNC_KILL") {
+        this.contentImportData = data.contentImportData;
         this.workerProcessRef.kill();
-      } else if(data.message === 'IMPORT_COMPLETE'){
-        this.cb(null, this.contentImportData);
+        await this.updateStatusInDB();
+      } else if (data.message === 'IMPORT_ERROR') {
+        this.cb(data.message, this.contentImportData);
+        console.log('handle error');
+      } else {
+        console.log('handle error');
+        this.cb('UNHANDLED_ERROR', this.contentImportData);
       }
     });
+  }
+  private async copyEcar(){
+    this.contentImportData.importStep = ImportSteps.parseEcar
+    await this.updateStatusInDB();
+    this.workerProcessRef.send({
+      message: this.contentImportData.importStep || ImportSteps.parseEcar,
+      contentImportData: this.contentImportData
+    });
+  }
+  async startImport(step = this.contentImportData.importStep) {
+    switch (step) {
+      case ImportSteps.copyEcar: {
+        this.workerProcessRef.send({
+          message: this.contentImportData.importStep,
+          contentImportData: this.contentImportData
+        });
+        break;
+      }
+      case ImportSteps.parseEcar: {
+        this.workerProcessRef.send({
+          message: this.contentImportData.importStep || ImportSteps.parseEcar,
+          contentImportData: this.contentImportData
+        });
+        break;
+      }
+      case ImportSteps.processManifest: {
+        this.processManifest()
+        break;
+      }
+      default: {
+        this.cb('UNHANDLED_ERROR', this.contentImportData);
+        break;
+      }
+    }
+  }
+
+  async updateStatusInDB() {
+    let importDbResults: IContentImport = _.find(contentImportDB, { id: this.contentImportData.id });
+    importDbResults = { ...importDbResults, ...this.contentImportData };
+  }
+
+
+  private async processManifest(contentImportData?) {
+    try {
+      if (contentImportData) {
+        this.contentImportData = contentImportData;
+        this.contentImportData.importStep = ImportSteps.processManifest
+        await this.updateStatusInDB();
+      }
+      this.manifest = JSON.parse(fs.readFileSync(contentFolder + '/' + this.contentImportData.id + '/manifest.json', 'utf8'));
+      let parent = _.get(this.manifest, 'archive.items[0]');
+      let resources = [];
+      if (_.get(parent, 'visibility') !== 'Default') {
+        throw 'INVALID_MANIFEST'
+      }
+      if (parent.compatibilityLevel > 1) { // config.get("CONTENT_COMPATIBILITY_LEVEL")
+        throw `UNSUPPORTED_COMPATIBILITY_LEVEL`;
+      }
+
+      const dbContents = await this.getContentsFromDB([parent.identifier]).catch(err => []);
+      if (dbContents && dbContents.length) {
+        // TODO: if content already exist in app 
+        // 1.check compatibility level 
+        // 2.compatibility level of childNodes of collection if update available update content
+        // 3.collection ecar has more content ecar that in app, add those missing content to app
+        this.cb(null, this.contentImportData);
+        return;
+      }
+      this.contentImportData.contentId = parent.identifier;
+      this.contentImportData.contentType = parent.mimeType;
+      if (this.contentImportData.contentType === 'application/vnd.ekstep.content-collection') {
+        let itemsClone = _.cloneDeep(_.get(this.manifest, 'archive.items'));
+        parent.children = this.createHierarchy(itemsClone, parent);
+        parent.baseDir = `content/${parent.identifier}`;
+        parent.desktopAppMetadata = {
+          // "ecarFile": resource.identifier + '.ecar',  // relative to ecar folder
+          "addedUsing": 'IMPORT',// IAddedUsingType.import,
+          "createdOn": Date.now(),
+          "updatedOn": Date.now(),
+        }
+        resources = _.filter(_.get(this.manifest, 'archive.items'), item => (item.mimeType !== 'application/vnd.ekstep.content-collection'))
+          .map(resource => {
+            resource.baseDir = `content/${resource.identifier}`;
+            resource.desktopAppMetadata = {
+              // "ecarFile": resource.identifier + '.ecar',  // relative to ecar folder
+              "addedUsing": 'IMPORT',// IAddedUsingType.import,
+              "createdOn": Date.now(),
+              "updatedOn": Date.now(),
+            }
+            resource.appIcon = resource.appIcon ? `content/${resource.appIcon}` : resource.appIcon;
+            return resource;
+          });
+      }
+      this.contentImportData.importStep = ImportSteps.extractEcar;
+      this.contentImportData.extractedEntries = { 'manifest.json': true }
+      await this.updateStatusInDB();
+      this.workerProcessRef.send({
+        message: this.contentImportData.importStep,
+        contentImportData: this.contentImportData
+      });
+      this.cb(null, this.contentImportData);
+    } catch (err) {
+      this.cb(null, this.contentImportData);
+    }
+  }
+  private async getContentsFromDB(contentIds: Array<string>) {
+    return Promise.resolve([]);
   }
 
   async resume() {
@@ -243,12 +360,38 @@ class ImportEcar {
     console.log('pause import');
     this.workerProcessRef.send({ message: 'KILL' });
   }
+  createHierarchy(items: any[], parent: any, reqID?: any, tree?: any[]): any {
+    console.debug(`ReqId = "${reqID}": creating Hierarchy for the Collection`);
+    console.info(` ReqId = "${reqID}": Getting child contents for Parent: ${_.get(parent, 'identifier')}`);
+    tree = typeof tree !== 'undefined' ? tree : [];
+    parent = typeof parent !== 'undefined' ? parent : { visibility: 'Default' };
+    if (parent.children && parent.children.length) {
+      let children = [];
+      _.forEach(items, (child) => {
+        let childWithIndex = _.find(parent.children, { 'identifier': child.identifier })
+        if (!_.isEmpty(childWithIndex)) {
+          child.index = childWithIndex['index'];
+          children.push(child)
+        }
+      });
+      if (!_.isEmpty(children)) {
+        children = _.sortBy(children, 'index');
+        if (parent.visibility === 'Default') {
+          tree = children;
+        } else {
+          parent['children'] = children;
+        }
+        _.each(children, (child) => { this.createHierarchy(items, child, reqID) });
+      }
+    }
+    console.info(` ReqId = "${reqID}": Child contents are found for Parent: ${_.get(parent, 'identifier')}`);
+    return tree;
+  }
 }
 process.on('unhandledRejection', err => {
   console.log('unhandledRejection', err);
 });
 
 process.on('uncaughtException', function (err) {
-  console.log('uncaughtException', err);
-  process.exit(1);
+  console.log('uncaughtException caught in contentImportManager', err);
 })
