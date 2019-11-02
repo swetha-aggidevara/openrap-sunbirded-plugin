629e08549a3b3c7fc00830c570f1b168e3af3092
Anoop HM <anoophm44@gmail.com>
2019-11-02 22:09:39 +0530

    Issue #SB-13912 feat: export content refactor to sync file structure with content import

--- server/manager/contentExportMangaer/contentExport.ts
+++ server/manager/contentExportMangaer/contentExport.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as archiver from 'archiver';
 import * as path from 'path';
 import * as  _ from 'lodash';
-
+import * as fse from 'fs-extra';
 const createDirect = async (path) => {
   if (!fs.existsSync(path)){
     fs.mkdirSync(path);
@@ -12,25 +12,40 @@ createDirect('temp');
 
 class ExportContent {
   tempBaseFolder = 'temp';
-  contentBaseFolder = 'content';
+  contentBaseFolder = 'content_1';
   parentArchive;
   parentManifest;
   parentDetails;
   ecarName;
-  constructor(public contentId = 'do_31275124190583193617067'){
+  interval;
+  corruptContents = [];
+  startTime = Date.now();
+  cb;
+  constructor(public contentId = 'do_31275124190583193617067' || 'do_31276383142505676814236'){
     this.parentArchive = archiver('zip', { zlib: { level: 9 }});
   }
-  public async export(){
-    this.parentManifest = JSON.parse(fs.readFileSync(path.join(this.contentBaseFolder, this.contentId,  'manifest.json'), 'utf8'))
-    this.parentDetails = _.get(this.parentManifest, 'archive.items[0]');
-    this.ecarName =  this.parentDetails.name;
-    console.log('--------exporting content of mimeType--------', this.parentDetails.mimeType);
-    if (this.parentDetails.mimeType === 'application/vnd.ekstep.content-collection') {
-      await this.loadParentCollection();
-    } else {
-      await this.loadContent(this.parentDetails, false);
+  join(...paths){
+    return path.join(...paths);
+  }
+  public async export(cb){
+    this.cb = cb;
+    try {
+      this.parentManifest = await fse.readJson(this.join(this.contentBaseFolder, this.contentId,  'manifest.json'));
+      this.parentDetails = _.get(this.parentManifest, 'archive.items[0]');
+      this.ecarName =  this.parentDetails.name;
+      console.log('--------exporting content of mimeType--------', this.parentDetails.mimeType);
+      if (this.parentDetails.mimeType === 'application/vnd.ekstep.content-collection') {
+        await this.loadParentCollection();
+      } else {
+        await this.loadContent(this.parentDetails, false);
+      }
+      this.interval = setInterval(() => console.log(this.parentArchive.pointer(), this.parentArchive._entriesCount, this.parentArchive._entriesProcessedCount), 1000);
+      const data = await this.streamZip();
+      this.cb(null, data);
+    } catch(error) {
+      this.cb(error, null);
+      console.log('Got Error while exporting content', this.ecarName, error);
     }
-    this.streamZip();
   }
   archiveAppend(type, src, dest){
     console.log(`Adding ${src} of type ${type} to dest folder ${dest}`);
@@ -45,21 +60,42 @@ class ExportContent {
       this.parentArchive.append(null, { name: dest});
     }
   }
+  async validContent(contentDetails){
+    if(contentDetails.appIcon){
+      const appIconFileName = path.basename(contentDetails.appIcon);
+      const appIcon = this.join(this.contentBaseFolder, contentDetails.identifier, appIconFileName);
+      const exist = await fse.pathExists(appIcon);
+      if(!exist){
+        return { valid: false, reason: 'APP_ICON_MISSING'};
+      }
+    }
+    if(contentDetails.artifactUrl && path.extname(contentDetails.artifactUrl) && path.extname(contentDetails.artifactUrl) !== '.zip'){
+      const artifactUrlName = path.basename(contentDetails.artifactUrl);
+      const artifactUrlPath = this.join(this.contentBaseFolder, contentDetails.identifier, artifactUrlName);
+      const exist = await fse.pathExists(artifactUrlPath);
+      if(!exist){
+        return { valid: false, reason: 'ARTIFACT_URL_MISSING'};
+      }
+    }
+    return { valid: true };
+  }
   async loadContent(contentDetails, child){
-
+    const contentState = await this.validContent(contentDetails);
+    if(!contentState.valid){
+      this.corruptContents.push({ id: contentDetails.identifier, reason: contentState.reason});
+      return ;
+    }
     const baseDestPath = child ? contentDetails.identifier + '/' : '';
     if(child){
-      console.log('-------dir name created----------', contentDetails.identifier);
       this.archiveAppend('createDir', null, contentDetails.identifier);
     }
-    this.archiveAppend('path', path.join('content', contentDetails.identifier, 'manifest.json'), baseDestPath + 'manifest.json');
+    this.archiveAppend('path', this.join(this.contentBaseFolder, contentDetails.identifier, 'manifest.json'), baseDestPath + 'manifest.json');
     if(contentDetails.appIcon){
       if(path.dirname(contentDetails.appIcon) !== '.'){
-        console.log('-------dir name created----------', baseDestPath +  path.dirname(contentDetails.appIcon));
         this.archiveAppend('createDir', null, baseDestPath +  path.dirname(contentDetails.appIcon));
       }
       const appIconFileName = path.basename(contentDetails.appIcon);
-      const appIcon = path.join('content', contentDetails.identifier, appIconFileName);
+      const appIcon = this.join(this.contentBaseFolder, contentDetails.identifier, appIconFileName);
       this.archiveAppend('path', appIcon, baseDestPath + contentDetails.appIcon);
     }
     // if(contentDetails.artifactUrl && path.dirname(contentDetails.artifactUrl) !== '.'){ // not needed as appIcon and artifact url will be in same folder
@@ -69,7 +105,7 @@ class ExportContent {
     // }
     if(contentDetails.artifactUrl && path.extname(contentDetails.artifactUrl) && path.extname(contentDetails.artifactUrl) !== '.zip'){
       const artifactUrlName = path.basename(contentDetails.artifactUrl);
-      const artifactUrlPath = path.join('content', contentDetails.identifier, artifactUrlName);
+      const artifactUrlPath = this.join(this.contentBaseFolder, contentDetails.identifier, artifactUrlName);
       this.archiveAppend('path', artifactUrlPath, baseDestPath + contentDetails.artifactUrl);
     } else if(contentDetails.artifactUrl && path.extname(contentDetails.artifactUrl) && path.extname(contentDetails.artifactUrl) === '.zip'){
       await this.loadZipContent(contentDetails, true);
@@ -78,14 +114,14 @@ class ExportContent {
   async loadZipContent(contentDetails, child){
     const baseDestPath = child ? contentDetails.identifier + '/' : '';
     const childArchive = archiver('zip', { zlib: { level: 9 }});
-    const toBeZipped: any = await this.readDirectory(path.join('content', contentDetails.identifier));
+    const toBeZipped: any = await this.readDirectory(this.join(this.contentBaseFolder, contentDetails.identifier));
     for(const items of toBeZipped){
       console.log('--------------loadZipContent------------', items)
       if(!contentDetails.appIcon.includes(items) && items !== 'manifest.json'){
         if(path.extname(items)){
-          childArchive.append(fs.createReadStream(path.join('content', contentDetails.identifier, items)), { name: items });
+          childArchive.append(fs.createReadStream(this.join(this.contentBaseFolder, contentDetails.identifier, items)), { name: items });
         } else {
-          childArchive.directory(path.join('content', contentDetails.identifier, items), items);
+          childArchive.directory(this.join(this.contentBaseFolder, contentDetails.identifier, items), items);
         }
       }
     }
@@ -95,20 +131,20 @@ class ExportContent {
   async loadParentCollection(){
     if(this.parentDetails.appIcon){
       const appIconFileName = path.basename(this.parentDetails.appIcon);
-      const appIcon = path.join('content', this.parentDetails.identifier, appIconFileName);
+      const appIcon = this.join(this.contentBaseFolder, this.parentDetails.identifier, appIconFileName);
       this.archiveAppend('path', appIcon, this.parentDetails.appIcon);
       if(path.dirname(this.parentDetails.appIcon) !== '.'){
         console.log('-------dir name created----------', path.dirname(this.parentDetails.appIcon));
         this.archiveAppend('createDir', null, path.dirname(this.parentDetails.appIcon));
       }
     }
-    const collectionItems: any = await this.readDirectory(path.join('content', this.parentDetails.identifier));
+    const collectionItems: any = await this.readDirectory(this.join(this.contentBaseFolder, this.parentDetails.identifier));
     for(const items of collectionItems){
       if(!this.parentDetails.appIcon.includes(items)){
         if(path.extname(items)){
-          this.archiveAppend('path', path.join('content', this.parentDetails.identifier, items), items);
+          this.archiveAppend('path', this.join(this.contentBaseFolder, this.parentDetails.identifier, items), items);
         } else {
-          this.archiveAppend('directory', path.join('content', this.parentDetails.identifier, items), items);
+          this.archiveAppend('directory', this.join(this.contentBaseFolder, this.parentDetails.identifier, items), items);
         }
       }
     }
@@ -124,35 +160,42 @@ class ExportContent {
         item => (item.mimeType !== 'application/vnd.ekstep.content-collection'))
         .map(item => item.identifier)
     for(const child of childNodes){
-      const childManifest = JSON.parse(fs.readFileSync(path.join(this.contentBaseFolder, child,  'manifest.json'), 'utf8'));
-      const childDetails = _.get(childManifest, 'archive.items[0]');
-      await this.loadContent(childDetails, true);
+      const childManifest = await fse.readJson(this.join(this.contentBaseFolder, child,  'manifest.json'))
+      .catch(err => {
+        console.log('got error while reading content', child);
+        this.corruptContents.push({ id: child, reason: 'MANIFEST_MISSING'});
+      });
+      if(childManifest){
+        const childDetails = _.get(childManifest, 'archive.items[0]');
+        await this.loadContent(childDetails, true);
+      }
     }
   }
-  streamZip(){
-    let output = fs.createWriteStream(path.join('temp', this.ecarName + '.zip'));
-    output.on('close', () => {
-      console.log(this.parentArchive.pointer() + ' total bytes');
-      console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
-
-    output.on('end', () => {
-      console.log('Data has been drained');
-    });
-
-    this.parentArchive.on('warning', (err) => {
-      if (err.code === 'ENOENT') {
-        // log warning
-      } else {
-        // throw error
-        throw err;
-      }
-    });
-    this.parentArchive.on('error', function (err) {
-      throw err;
-    });
-    this.parentArchive.finalize();
-    this.parentArchive.pipe(output);
+  async streamZip(){
+    return new Promise((resolve, reject) => {
+      const ecarFilePath = this.join('temp', this.ecarName + '.zip');
+      let output = fs.createWriteStream(ecarFilePath);
+      output.on('close', () => {
+        clearInterval(this.interval);
+        console.log(this.parentArchive.pointer() + ' total bytes zipped');
+        console.log('Took ', (Date.now() - this.startTime)/1000, 'seconds');
+        console.log('Skipped corrupt content', this.corruptContents);
+        resolve({
+          ecarSize: this.parentArchive.pointer(),
+          timeTaken: (Date.now() - this.startTime)/1000,
+          skippedContent: this.corruptContents,
+          ecarFilePath
+        });
+      });
+      output.on('end', () => {
+        console.log('Data has been drained');
+      });
+      this.parentArchive.on('error', (err) => {
+        reject(err);
+      });
+      this.parentArchive.finalize();
+      this.parentArchive.pipe(output);
+    })
   }
   async readDirectory(path){
     return new Promise((resolve, reject) => {
@@ -169,4 +212,10 @@ class ExportContent {
 
 const exportMan = new ExportContent();
 
-exportMan.export();
+exportMan.export((err, suc) => {
+  if(err){
+    console.log('Export error: ', err.message);
+    return;
+  }
+  console.log('Ecar exported successfully', suc);
+});
