b6c71b63d34f112596bcf24e75e47c5548f06f7d
Anoop HM <anoophm44@gmail.com>
2019-11-18 12:11:33 +0530

    Issue #SB-15816 feat: FAQ api initial commit

--- server/controllers/faqs.ts
+++ server/controllers/faqs.ts
@@ -1,6 +1,5 @@
 import DatabaseSDK from "../sdk/database/index";
 import { Inject } from "typescript-ioc";
-import * as fs from "fs";
 import * as path from "path";
 import { Manifest } from "@project-sunbird/ext-framework-server/models";
 import * as glob from "glob";
@@ -10,46 +9,54 @@ import { logger } from "@project-sunbird/ext-framework-server/logger";
 import { containerAPI } from "OpenRAP/dist/api";
 import { HTTPService } from "@project-sunbird/ext-framework-server/services";
 const FAQS_DB = 'faqs';
-const FAQ_BLOB_URL = 'http://localhost:3000/faqs/' || `${process.env.APP_BASE_URL}blob/faqs/`;
+const FAQ_BLOB_URL = `${process.env.FAQ_BLOB_URL}`;
 
 export class Faqs {
 
   @Inject private databaseSdk: DatabaseSDK;
   private fileSDK;
+  private faqsBasePath;
   constructor(manifest: Manifest) {
       this.databaseSdk.initialize(manifest.id);
       this.fileSDK = containerAPI.getFileSDKInstance(manifest.id);
+      this.faqsBasePath = this.fileSDK.getAbsPath(path.join("data", "faqs"));
   }
   public async insert() {
-    let faqsFiles = this.fileSDK.getAbsPath(path.join("data", "faqs", "**", "*.json"));
-    let files = glob.sync(faqsFiles, {});
+    let files = glob.sync(path.join(this.faqsBasePath, "**", "*.json"), {});
     logger.log('--Inserting faqs to db--', files);
     for (let file of files) {
-      let faqs = await this.fileSDK.readJSON(file);
+      let faqsData: IFaqsData = await this.fileSDK.readJSON(file);
       let _id = path.basename(file, path.extname(file));
-      await this.addToDb(_id, _.get(faqs, 'faqs'));
+      await this.addToDb(_id, faqsData);
     }
   }
   public async read(req, res){
     const language = req.params.language;
     logger.info(`Got Faqs read request for language:`, req.params.language, `for ReqId: ${req.get('x-msgid')}`);
-    try {
-      let faqs = await this.fetchOnlineFaqs(language, req) || await this.fetchOfflineFaqs(language, req);
-      if(!faqs){
-        throw { status : 404, message: 'Faqs not found for requested language'}
-      }
+    let faqs = await this.fetchOnlineFaqs(language, req) || await this.fetchOfflineFaqs(language, req);
+    if(faqs){
       res.send(Response.success("api.faqs.read", { faqs }, req));
-    } catch(err) {
-      logger.error(`Got error while fetching Faq for language: `, language, `for ReqId: ${req.get('x-msgid')}, error message: `, err.message);
-      let status = err.status || 500;
-      res.status(status).send(Response.error("api.faqs.read", status));
+    } else {
+      logger.error(`Got error while fetching Faq for language: `, language, `for ReqId: ${req.get('x-msgid')} `);
+      res.status(404).send(Response.error("api.faqs.read", 404));
     }
   }
-  async fetchOfflineFaqs(language, req): Promise<Array<IFaqs> | undefined> {
+  async fetchOfflineFaqs(language, req): Promise<IFaqsData | undefined > {
     logger.info(`Getting faqs from db for language:`, language, `for ReqId: ${req.get('x-msgid')}`);
-    return this.databaseSdk.get(FAQS_DB, language).then(doc => doc.data);
+    let faqsData: IFaqsData = await this.databaseSdk.get(FAQS_DB, language).then(doc => doc.data).catch(err => {
+      logger.error(`Got error while reading Faq from DB for language`, language, `for ReqId: ${req.get('x-msgid')}, error message `, err.message);
+      return undefined;
+    });
+    if(!faqsData){ // Load from files. Not needed as we have inserted all faqs json on app start.
+      logger.info(`Getting faqs from file system for language:`, language, `for ReqId: ${req.get('x-msgid')}`);
+      faqsData = await this.fileSDK.readJSON(path.join(this.faqsBasePath, language + ".json")).catch(err => {
+        logger.error(`Got error while reading Faq from file for language`, language, `for ReqId: ${req.get('x-msgid')}, error message `, err.message);
+        return undefined;
+      })
+    }
+    return faqsData;
   }
-  async fetchOnlineFaqs(language, req): Promise<Array<IFaqs>  | undefined> {
+  async fetchOnlineFaqs(language, req): Promise<IFaqsData  | undefined > {
     logger.info(`Getting faqs from blob for language:`, language, `for ReqId: ${req.get('x-msgid')}`);
     const config = {
       headers: {
@@ -57,24 +64,21 @@ export class Faqs {
           "content-type": "application/json"
       }
     };
-    return await HTTPService.get(`${FAQ_BLOB_URL}${language}.json`, config).toPromise()
+    return await HTTPService.get(`${FAQ_BLOB_URL}faq-${language}.json`, config).toPromise()
     .then((data: any) => {
-      const faqs = _.get(data, 'data.faqs');
-      if(faqs){
-        this.addToDb(language, faqs);
+      const faqsData = _.get(data, 'data');
+      if(faqsData){
+        this.addToDb(language, faqsData);
       }
-      return faqs;
+      return faqsData;
     }).catch(err => {
       logger.error(`Got error while reading Faq from blob for language`, language, `for ReqId: ${req.get('x-msgid')}, error message `, err.message);
       return undefined;
     });
   }
-  private async addToDb(_id, data){
-    let doc = {
-      data
-    };
-    await this.databaseSdk.upsert(FAQS_DB, _id, doc)
-    .catch(err => logger.error(`Received error while insert/updating the ${_id} to faqs database and err.message: ${err.message}`));
+  private async addToDb(_id: string, data: IFaqsData){
+    await this.databaseSdk.upsert(FAQS_DB, _id, { data })
+    .catch(err => logger.error(`Received error while insert/updating faqs for language: ${_id} to faqs database and err.message: ${err.message}`));
   }
 }
 
@@ -82,4 +86,7 @@ export interface IFaqs {
   topic: string;
   description: string;
 }
-
+export interface IFaqsData {
+  faqs: Array<IFaqs>;
+  constants: Object;
+}
