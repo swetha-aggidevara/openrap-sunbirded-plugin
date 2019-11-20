80aa6bde9b79cf4dc1b63295eab7c6f55eee329e
Harishkumar Gangula <harishg@ilimi.in>
2019-11-20 11:37:52 +0530

    Issue #SB-15854 fix: skipped some tests

--- server/routes.spec.ts
+++ server/routes.spec.ts
@@ -1062,7 +1062,7 @@ describe('Update content', () => {
 
 describe.skip('Test Export Content/Collection', () => {
 
-    it.skip('#Export Content', (done) => {
+    it('#Export Content', (done) => {
         supertest(app)
             .get('/api/content/v1/export/KP_FT_1564394134764')
             .set('Accept', 'application/json')
