52a92c08c0c9a027d1f459aab585ef7e996ac8cb
Anoop HM <anoophm44@gmail.com>
2019-12-24 16:25:35 +0530

    Issue #SB-16473 fix: online content bug fix

--- server/controllers/content/contentHelper.ts
+++ server/controllers/content/contentHelper.ts
@@ -87,7 +87,9 @@ export const addContentListener = (pluginId) => {
                         await fileSDK.move(path.join('content', fileName, metaData.appIcon), path.join('content', fileName, appIconFileName))
                         folderToDelete.push(path.join('content', fileName, path.dirname(metaData.appIcon)))
                     }
-                    if(metaData.artifactUrl && path.extname(metaData.artifactUrl) && path.extname(metaData.artifactUrl) !== '.zip'){
+                    if(!metaData.artifactUrl || (metaData.artifactUrl && metaData.contentDisposition === 'online')) {
+                        // do nothing
+                    } else if(metaData.artifactUrl && path.extname(metaData.artifactUrl) && path.extname(metaData.artifactUrl) !== '.zip'){
                         const artifactName = path.basename(metaData.artifactUrl);
                         await fileSDK.move(path.join('content', fileName, metaData.artifactUrl), path.join('content', fileName, artifactName))
                         folderToDelete.push(path.join('content', fileName, path.dirname(metaData.artifactUrl)))
