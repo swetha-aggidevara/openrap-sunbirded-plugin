69ce8307bd3841583c7da49a5b9daeb5648ce58b
Harishkumar Gangula <harishg@ilimi.in>
2020-02-05 15:40:33 +0530

    Issue #SB-17240 feat: delete action is moved to system queue

--- server/controllers/content/contentDelete.ts
+++ server/controllers/content/contentDelete.ts
@@ -2,25 +2,24 @@ import { logger } from "@project-sunbird/ext-framework-server/logger";
 import { Manifest } from "@project-sunbird/ext-framework-server/models";
 import * as childProcess from "child_process";
 import * as _ from "lodash";
-import { containerAPI } from "OpenRAP/dist/api";
+import { containerAPI, ISystemQueueInstance, SystemQueueReq } from "OpenRAP/dist/api";
 import * as path from "path";
 import * as TreeModel from "tree-model";
 import { Inject } from "typescript-ioc";
 import DatabaseSDK from "../../sdk/database";
 import Response from "../../utils/response";
-import Content from "./content";
-import { IContentDelete, IDeletePath } from './IContent';
+import { ContentDeleteHelper } from "./contentDeleteHelper";
+import { IContentDelete, IDeletePath } from "./IContent";
 
 export default class ContentDelete {
     private workerProcessRef: childProcess.ChildProcess;
     @Inject
     private databaseSdk: DatabaseSDK;
-    private fileSDK: any;
-    private content: Content;
+    private systemQueue: ISystemQueueInstance;
     constructor(manifest: Manifest) {
-        this.content = new Content(manifest);
         this.databaseSdk.initialize(manifest.id);
-        this.fileSDK = containerAPI.getFileSDKInstance(manifest.id);
+        this.systemQueue = containerAPI.getSystemQueueInstance(manifest.id);
+        this.systemQueue.register(ContentDeleteHelper.taskType, ContentDeleteHelper);
     }
 
     public async delete(req, res) {
@@ -32,9 +31,6 @@ export default class ContentDelete {
             return res.status(400).send(Response.error(`api.content.delete`, 400, "MISSING_CONTENTS"));
         }
         try {
-            if (!this.workerProcessRef) {
-                this.workerProcessRef = childProcess.fork(path.join(__dirname, "contentDeleteHelper"));
-            }
             const failed: object[] = [];
             const dbFilter = {
                     selector: {
@@ -51,13 +47,13 @@ export default class ContentDelete {
                     failed.push(err.message || err.errMessage);
             });
             deleted =  _.map(deleted, (content) => content.id);
-            const contentPaths: IDeletePath[] = _.map(deleted, (id) => {
+            const contentPaths: string[] = _.map(deleted, (id) => {
                 if (id) {
-                    return ({ path: path.join("content", id) });
+                    return path.join("content", id);
                 }
             });
             if (contentPaths) {
-                this.workerProcessRef.send(contentPaths);
+                await this.add(contentPaths, contentsToDelete[0]["name"]);
             }
             res.send(Response.success("api.content.delete", {deleted, failed}, req));
             } catch (err) {
@@ -65,6 +61,19 @@ export default class ContentDelete {
                 res.status(500);
                 res.send(Response.error(`api.content.delete`, 500, err.errMessage || err.message, err.code));
             }
+    }
+
+    public async add(contentDeletePaths: string[], name): Promise<string[]> {
+        logger.info("add started for ", contentDeletePaths);
+        const insertData: SystemQueueReq = {
+            type: ContentDeleteHelper.taskType,
+            name,
+            metaData: {
+              filePaths: contentDeletePaths,
+            },
+        };
+        const ids = await this.systemQueue.add(insertData);
+        return ids;
     }
 
     public async getContentsToDelete(contentsToDelete: IContentDelete[]): Promise <IContentDelete[]> {
@@ -90,8 +99,8 @@ export default class ContentDelete {
         const model = new TreeModel();
         let treeModel;
         treeModel = model.parse(content);
-        treeModel.walk(node => {
-            if (node.model.mimeType !== 'application/vnd.ekstep.content-collection') {
+        treeModel.walk((node: { model: { mimeType: string; identifier: string; }; }) => {
+            if (node.model.mimeType !== "application/vnd.ekstep.content-collection") {
                 resourceIds.push(node.model.identifier);
             }
         });
@@ -118,35 +127,5 @@ export default class ContentDelete {
         };
         logger.info(`finding all child contents of a collection`);
         return await this.databaseSdk.find("content", dbFilter);
-    }
-
-    public async reconciliation() {
-        try {
-            logger.debug(`deleteReconciliation() is called`);
-            if (!this.workerProcessRef) {
-                this.workerProcessRef = childProcess.fork(path.join(__dirname, "contentDeleteHelper"));
-            }
-            const dbFilter = {
-                selector: {
-                    "desktopAppMetadata.isAvailable": false,
-                },
-            };
-            logger.info(`finding all contentsToDelete in Queue `);
-            const contents = await this.databaseSdk.find("content", dbFilter).catch((error) => {
-                logger.error(`Received Error while finding contents (isAvailable : false) Error: ${error.stack}`);
-            });
-            const contentsToDelete: IContentDelete[] = await this.getContentsToDelete(contents.docs);
-            const deleted = await this.databaseSdk.bulk("content", contentsToDelete);
-            const contentPaths: IDeletePath[] = _.map(deleted, (content) => {
-                if (!_.isEmpty(_.get(content, "id"))) {
-                    return ({ path: path.join("content", content.id) });
-                }
-            });
-            if (contentPaths) {
-                this.workerProcessRef.send(contentPaths);
-            }
-        } catch (err) {
-            logger.error(`Received Error While deleting contents In reconciliation() Error: ${err.stack}`);
-        }
     }
 }
